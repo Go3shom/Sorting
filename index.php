1fa620a1769acb4b49abc3ccbcd8df15cdd9fb83
Go3shom <shadi.muhammad001@gmail.com>
2019-03-14 18:52:55 +0200

    Modified Index file

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="utf-8">
     <title>LAST</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        table { border-collapse: collapse; width: 85%; color: #010101; font-family: 'Roboto'; font-size: 20px; text-align: center; margin: 30px auto; }
-        th {  padding: 10px; background-color: #69779b;}
+      <style>
+        body { color: #010101; font-family: 'Roboto'; font-size: 20px; text-align: center; margin: 30px auto; }
+        a { color: #010101; text-decoration: none; text }
+        a:hover { color: #69779b; }
+        table { border-collapse: collapse; width: 85%; text-align: center; margin: 30px auto; }
+        th { padding: 10px; background-color: #69779b; }
         th > a { color: #F0ECE2; text-decoration: none; }
         th > a:hover { color: #010101; }
         tr:nth-child(even) { background-color: #F0ECE2; }
@@ -14,76 +17,62 @@
 </head>
 <body>
     <?php
-      $con = new mysqli( "localhost", "root", "", "targetdb" );
-      if ( $con->connect_error ) {
-          die( "Connection Error" . $con->connect_error );
-      }
-        
-      // function sort_headers( $sort_field, $sort_order ) {
-      $sort_field = 'ClientID';
-
-      if ( isset( $_GET[ 'sort_field' ] ) ) {
+    $con = new mysqli( "localhost", "root", "", "targetdb" );
+    
+    if ( $con->connect_error ) {
+        die( "Failed" . connection_error() );
+    }
+    
+    if ( isset( $_GET[ 'sort_field' ] ) ) {
         $sort_field = $_GET[ 'sort_field' ];
-      }
-      elseif ( $sort_field == [ 'ClientID' ] ) {
-        $sort_field = 'ClientID';
-      } 
-      elseif( $sort_field == [ 'ClientName' ] ) {
-        $sort_field = 'ClientName';
-      } 
-      elseif( $sort_field == [ 'ClientAddress' ] ) {
-        $sort_field = 'ClientAddress';
-      }
-    // }
+    } else {
+        $sort_field = 'clientID';
+    }
     
-      if ( isset( $_GET[ 'sort_order' ] ) ) {
-          $sort_order = $_GET[ 'sort_order' ];
-      } else {
-          $sort_order = 'ASC';
-      }
-      
-      $sort_order == 'DESC' ? $sort_order = 'ASC' : $sort_order = 'DESC';
+    if ( isset( $_GET[ 'sort_order' ] ) ) {
+        $sort_order = $_GET[ 'sort_order' ];
+    } else {
+        $sort_order = 'ASC';
+    }
 
+    echo "<a href='/sorting'>Home</a>";
 
-      $sql = "SELECT ClientID, ClientName, ClientAddress FROM Clients";
-      // $sql = "SELECT ClientID FROM Clients";
-      // $sql.= " ORDER BY $sort_field";
-      $sql.= " ORDER BY $sort_field $sort_order";
+    $sql = " SELECT ClientID, ClientName, ClientAddress FROM Clients ";
 
-      $result = $con->query( $sql );
-
-        // sort_headers( $sort_field, $sort_order );
-      
-      echo "<table><thead><tr>";
-
-        echo "<th><a href='?sort_field=$sort_field&&sort_order=$sort_order'>ClientID</a></th>";
-        echo "<th><a href='?sort_field=$sort_field&&sort_order=$sort_order'>ClientName</a></th>";
-        echo "<th><a href='?sort_field=$sort_field&&sort_order=$sort_order'>ClientAddress</a></th>";
+    $sql .= " ORDER BY $sort_field $sort_order";
     
-      echo "</tr></thead>";
+    $result = $con->query( $sql );
+    
+    
+    if ( $result->num_rows > 0 ) {
 
-      if ( $result->num_rows > 0 ) {
-          // $sort_order == 'DESC' ? $sort_order = 'ASC' : $sort_order = 'DESC';
-          // var_dump($sort_order);
-          
-          while ( $row = $result->fetch_assoc() ) {
-              echo "<td>";
-              // $clientID = $row['ClientID'];
-              echo $row['ClientID'];
-              echo "</td><td>";
-              // $clientName = $row['ClientName'];
-              echo $row['ClientName'];
-              echo "</td><td>";
-              // $clientAddress = $row['ClientAddress'];
-              echo $row['ClientAddress'];
-              echo "</td></tr>";
-          }
-            echo "</tbody></table>";
-        }
+        $sort_order == 'DESC' ? $sort_order = 'ASC' : $sort_order = 'DESC';
+    
+        echo "
+          <table>
+            <thead>
+                <tr>
+                    <th><a href='?sort_field=clientid&&sort_order=$sort_order'>Client ID</a></th>
+                    <th><a href='?sort_field=clientName&&sort_order=$sort_order'>Client Name</a></th>
+                    <th><a href='?sort_field=clientAddress&&sort_order=$sort_order'>Client Address</a></th>
+                </tr>
+            </thead><tbody>";
         
-        $con->close();
+        while ( $rows = $result->fetch_assoc() ) {
+         echo "<tr><td>";
+            echo $rows[ 'ClientID' ];
+            echo "</td><td>";
+            echo $rows[ 'ClientName' ];
+            echo "</td><td>";
+            echo $rows[ 'ClientAddress' ];
+         echo "</td></tr>";
+        }
+    } else {
+        echo "No Result Returned.";
+    }
+    echo "</tbody></table>";
 
-    /******************************************************/
+    $con->close();
 
     ?>
 </body>
